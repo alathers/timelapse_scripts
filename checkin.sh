d72cb913ec3aaa7d95e11c828d4d14b362060842
Adam Lathers <alathers@gmail.com>
2020-09-16 09:55:27 -0700

    Make a runlog to avoid killing mailq

--- checkin.sh
+++ checkin.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# not in the configs because the script needs it to find them and I'm too lazy to do the "run from" logic
+timelapse_src='/home/pi/src/timelapse_scripts'
+pushd ${timelapse_src}
+
 . ./config.generic
 
 . ./config
 
-
+git pull
+git add runlog
+git commit -m "Updating Runlog"
+git push
